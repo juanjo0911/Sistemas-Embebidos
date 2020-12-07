a84d6bbbd73bf7937f358b2ff95f2d76d0971727
juanjo0911 <74636363+juanjo0911@users.noreply.github.com>
2020-12-07 09:40:31 -0500

    Update Vasquez_Juan_Prueba1_SE.ino

--- Vasquez_Juan_Prueba1_SE.ino
+++ Vasquez_Juan_Prueba1_SE.ino
@@ -300,23 +300,15 @@ void entrada(){
   
   switch(ent){
     case 0:
-    lcd.setCursor(0,0);
-    lcd.print("                ");
-    delay(20);
-    lcd.setCursor(0,0);
-    lcd.print("INICIO");
+    Serial.println("INICIO")
     ent++;
     break;
     case 1:
-    lcd.setCursor(0,1);
-    lcd.print("                ");
-    delay(20);
-    lcd.setCursor(0,1);
-    lcd.print("SALIDA");
+    Serial.println("SALIDA");
     ent++;
     break;
     default:
-    lcd.clear();
+    
     ent=0;
     break;
   }
